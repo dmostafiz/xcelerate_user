81aab1b8ea4849f48d87b9805208be0451e98a7e
dmostafiz <dev.mostafiz@gmail.com>
2024-06-25 00:52:56 +0600

    first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+
+    <div v-if="accessToken">You are logged in</div>
+    <div v-else>Ooops! Not authenticated.</div>
+
+  </div>
+</template>
+
+<script setup>
+
+const { accessToken } = useCookie()
+
+</script>
