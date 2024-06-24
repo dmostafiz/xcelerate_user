4e559c6affe8c985ae5c3b5328d7018a064f2014
dmostafiz <dev.mostafiz@gmail.com>
2024-06-25 00:56:41 +0600

    okay

--- app.vue
+++ app.vue
@@ -5,11 +5,12 @@
     <div v-if="accessToken">You are logged in</div>
     <div v-else>Ooops! Not authenticated.</div>
 
+    <div>AT: {{ accessToken }}</div>
   </div>
 </template>
 
 <script setup>
 
-const { accessToken } = useCookie()
+const accessToken = useCookie('accessToken')
 
 </script>
