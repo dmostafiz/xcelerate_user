c28b72f3f6b563ea4a4015c3c8365f1b2926f300
dmostafiz <dev.mostafiz@gmail.com>
2024-06-25 23:12:26 +0600

    okay

--- stores/authStore.ts
+++ stores/authStore.ts
@@ -6,28 +6,24 @@ export const useAuthStore = defineStore({
 
   state: () => ({
     id: '',
-    uid: '',
     username: '',
     email: '',
     first_name: '',
     last_name: '',
-    display_name: '',
+    full_name: '',
     user_type: '',
-    account_type: '',
     avatar: '',
   }),
 
   actions: {
     addUser(user: any) {
       this.id = user.id
-      this.uid = user.uid
       this.username = user.username
       this.email = user.email
       this.first_name = user.first_name
       this.last_name = user.last_name
-      this.display_name = user.display_name
+      this.full_name = user.full_name
       this.user_type = user.user_type
-      this.account_type = user.account_type
       this.avatar = user.avatar
     },
 
@@ -37,14 +33,12 @@ export const useAuthStore = defineStore({
 
     removeUser() {
       this.id = ''
-      this.uid = ''
       this.username = ''
       this.email = ''
       this.first_name = ''
       this.last_name = ''
-      this.display_name = ''
+      this.full_name = ''
       this.user_type = ''
-      this.account_type = ''
       this.avatar = ''
     }
   },
